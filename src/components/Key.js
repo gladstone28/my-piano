edacd80e4eae91c31cc1925a73979c819cdcf0b5
Gladstone Rose <gladstonerose@hotmail.com>
2023-06-22 11:03:58 -0500

    initial additions of files

---
+++ src/components/Key.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Key extends React.Component {
+render () {
+return (
+<div>
+<h1>Key!</h1>
+
+</div>
+
+);
+
+}
+
+
+}
+
+export { Key };
---
+++ src/components/Piano.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Key } from './Key.js';
+
+class Piano extends React.Component {
+    render() {
+        return (
+	<div >
+        <Key />
+        <Key />
+        <Key />
+
+       </div >
+        );
+    }
+
+}
+
+export { Piano };
